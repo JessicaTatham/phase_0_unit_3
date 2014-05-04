17cb368cdf0349c92addf85b59b7ce38114fb387
Jessica Tatham <jectatham@gmail.com>
2014-05-04 14:04:15 -0700

    re-submit corrected JS groceryList

--- week_8_and_9/3_JavaScript/grocery_list/my_solution.js
+++ week_8_and_9/3_JavaScript/grocery_list/my_solution.js
@@ -22,41 +22,58 @@
 
 // 3. Initial Solution
 
-var groceryList = []; 
+var groceryList = function(list) {
+  this.list = list;
+  this.items = [];
+}
 
-groceryList.displayItem = groceryList ; 
+groceryList.prototype.addItem = function(item) {
+  this.items.push(item) ;
+}
+
+groceryList.prototype.deleteItem = function(item) { 
+  var index = this.items.indexOf(item);
+  this.items.splice(index,1)
+} 
+
+groceryList.prototype.displayItem = function() { 
+    for (i=0; i<this.items.length; i++){
+      console.log(this.items[i].list);
+    }
+} 
+
+var Item = function(name){
+  this.name = name;
+}
    
-groceryList.addItem = function(item) {
-  groceryList.push(item) } ;
-; 
 
 groceryList.numItems = groceryList.length ; 
 
-groceryList.deleteItem = function(index_of_item) { 
-  groceryList.splice(index_of_item) } ;
-; 
 
-// 4. Refactored Solution
+
 
 
 
 // 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
-groceryList.addItem('apple') === groceryList['apple'] 
-groceryList.displayItem
+var apple = new Item('apple')
+var milk = new Item('milk')
+var chicken = new Item('chicken')
+var grocery_1 = new groceryList('grocery_1');
+grocery_1.addItem(apple);
+grocery_1.addItem(milk);
+grocery_1.addItem(chicken);
+grocery_1.displayItem;
+grocery_1.deleteItem(apple);
+grocery_1.displayItem;
 
-function assert(test, message, test_number) {
-  if (!test) {
-    console.log(test_number + "false");
-    throw "ERROR: " + message;
-  }
-  console.log(test_number + "true");
-  return true;
-}
 
-assert(
-  groceryList.addItem('apple') === groceryList['apple']
-  "apple is added to groceryList. "
-)
+
 
 // 5. Reflection 
+
+// Jake and I really tried to tackle this challenge, with very little success. Eventually we had to take a step back from it, as I had previous commitments
+// that I needed to do. Later, going back and seeing how others found a solution to this challenge, I see that in some cases, such as the addItem
+// and driver tests, we were on the right track, but other than that we were way off. I had to do a lot of 'borrowing' from others solutions, but now
+// that I implemented their solutions into my previous solution, I feel like I have a firm understanding of how to go about completing this challenge. That
+// being said, I much prefered doing this challenge in Ruby. 
