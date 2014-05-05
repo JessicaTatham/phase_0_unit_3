d1c6f369f2387c47681e87c0232aa7b378e747eb
Jessica Tatham <jectatham@gmail.com>
2014-05-04 19:13:40 -0700

    Finished week 9 Ruby challenges

--- week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
+++ week_8_and_9/4_Ruby/rectangle_class/my_solution.rb
@@ -1,12 +1,15 @@
 # U3.W8-9: Implement a Rectangle Class
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
-
-
-
+=begin
+Define the method area that calculates the area by multiplying width by height.
+Define the method perimeter that calculates the parimeter by multiplying the width by 2 and the height by 2 and adding them together.
+Define the method diagonal that calculates the diagonal by finding the square root of height squared plus the width squared.
+Define the method square? that determines whether the rectangle is a square or not by checking if the width is equal to the height.
+=end
 # 3. Initial Solution
 class Rectangle
   attr_accessor :width, :height
@@ -20,6 +23,23 @@ class Rectangle
     (other.width  == self.width && other.height == self.height ) ||
     (other.height == self.width && other.width  == self.height )
   end
+
+  def area
+  	@width*@height
+  end
+
+  def perimeter
+  	@width*2+@height*2
+  end
+
+  def diagonal
+  	(@width ** 2 + @height ** 2) ** 0.5
+  end
+
+  def square?
+  	@width == @height
+  end
+
 end
 
 
@@ -27,16 +47,32 @@ end
 
 # 4. Refactored Solution
 
-
+#I don't see a way to refactor this code.
 
 
 
 
 # 1. DRIVER TESTS GO BELOW THIS LINE
 
+rectangle = Rectangle.new(5,10)
+square = Rectangle.new(5,5)
 
+p rectangle.area
+p rectangle.perimeter
+p rectangle.diagonal
+p rectangle.square?
+p square.area
+p square.perimeter
+p square.diagonal
+p square.square?
 
 
 
 
 # 5. Reflection 
+=begin
+I found this to be a very straight forward challenge. Defining each of the methods wasnt difficult and translating the 
+Rspec was simple. I did have to look up how to find the diagonal as my basic geometric measurements class failed me in 
+high school!
+
+=end
--- week_8_and_9/4_Ruby/reverse_words/my_solution.rb
+++ week_8_and_9/4_Ruby/reverse_words/my_solution.rb
@@ -1,29 +1,44 @@
 # U3.W8-9: Reverse Words
 
 
-# I worked on this challenge [by myself, with: ].
+# I worked on this challenge [by myself].
 
 # 2. Pseudocode
 
 
 
 # 3. Initial Solution
+=begin
+def reverse_words(sentence)
+	sentence.reverse
+end
 
-
-
-
+=end
 # 4. Refactored Solution
 
-
-
-
-
+def reverse_words(sentence)
+	sentence.split(' ').map {|word| word.reverse}.join(' ')
+end
 
 # 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
 
+p reverse_words("Hello") == "olleH"
 
+p reverse_words("Hakuna Matata") #== "anukaH atataM"
 
+p reverse_words("") == ""
+
+p reverse_words("How are you") #== "woH era uoy"
 
 
 
 # 5. Reflection 
+=begin
+	
+I found this to be a relatively easy challenge. My first solution was a little too simplified, and only worked when I wanted to reverse one 
+word or an empty string. My final solution, I just had to loop through the sentence to reverse each word separately. I didn't have to research
+any methods for this challenge, but I did have to take a look into others solutions to remember to use the split and join method to make the 
+solution work exactly right.
+	
+=end
+
