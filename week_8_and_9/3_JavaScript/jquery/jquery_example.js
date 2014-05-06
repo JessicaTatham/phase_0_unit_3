57dfd9b82031e325e51009ffa613d5f15d6653d9
Jessica Tatham <jectatham@gmail.com>
2014-05-05 19:06:09 -0700

    Finished JS JQuery challenge

--- week_8_and_9/3_JavaScript/jquery/jquery_example.js
+++ week_8_and_9/3_JavaScript/jquery/jquery_example.js
@@ -6,20 +6,48 @@ $(document).ready(function(){
 $('body').css({'background-color': 'pink'})
  
 //RELEASE 1:
-  //Add code here to select elements of the DOM 
+var bodyElement = $('body')
+var human = $('Jeff','Amanda')
  
  
 //RELEASE 2: 
-  // Add code here to modify the css and html of DOM elements
+$('h1').css({'border': '9px solid red', 'background-color':'white'}) 
+$('#mascot').html('Cicada')
+
  
  
 //RELEASE 3: Event Listener
-  // Add the code for the event listener here 
+  
+
+  $('img').on('mouseover', function(){
+    $(this).attr('src', 'http://adudesguide.com/wordpress/wp-content/uploads/2013/08/cicadas.jpg')
+   }) 
+
+  $('img').on('mouseleave', function(){
+  	$(this).attr('src', 'http://socialtimes.com/files/2013/02/Dev-Bootcamp-logo.jpg')
+  });
  
  
 //RELEASE 4 : Experiment on your own
  
- 
+ $('#mascot').click(function(){
+ 	$('img').animate({
+ 		height: "toggle"
+ 	});
+ })
+
+  $('img').click(function(){
+ 	$('img').animate({
+ 		width: "100px"
+ 	});
+ })
+
+  $('#mascot').click(function(){
+ 	$('#mascot').animate({
+ 		borderWidth: "30px"
+ 	});
+ })
+  
  
  
  
